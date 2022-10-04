8c0300130b26e8e82dc77fff91ed48fa65fc4ef1
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-10-04 16:38:09 -0400

    Created the python files

---
+++ App.py
@@ -0,0 +1,3 @@
+print('Welcome To the game')
+name = input("enter name ") #this code is used to get information from the user
+print("helo", name, sep=" ") #instead of using plus and adding spacing sep helped make it concise
