2c6385307ee3ca4776be7f179960bbf8a40ef8f4
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-10-06 10:12:04 -0400

    Milestone 3 in app created role coice and can display attributes

--- Game.py
+++ Game.py
@@ -1,4 +1,4 @@
-import random
+import random, Knight, Wizard, App
 class DiceOfFaith:
     def show(self):
         die1= random.randint(1,6)
--- Knight.py
+++ Knight.py
@@ -1,1 +1,8 @@
-strength= 4 #one of the attributes
+class KnightAtri:
+    def Arti(self):
+        strength = 3 #one of the attributes
+        Intelligeence = 5
+        Experience = 2
+        print("Your strength is", strength, "Your Intelligeence is", Intelligeence, "Your Experience is", Experience)
+
+    
