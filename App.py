139d94f4c928adfa94b4fc785d57ba3ccd970b1c
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-10-06 00:05:09 -0400

    Milestone 2: Game intro, Completed beginging print statements and choice to play or exit game

--- App.py
+++ App.py
@@ -1,5 +1,11 @@
 import Game, Knight, Wizard
 
-print('Welcome To the game')
-name = input("enter name ") #this code is used to get information from the user
-print("helo", name, sep=" ") #instead of using plus and adding spacing sep helped make it concise
+print("Welcome to Wonderland" + '\n' + "Where Dreams and Nightmares Thrive")
+playGame = input("Dare to enter? (Type yes or no) ")
+if playGame == "yes":
+    print('\n' + "Nightmares and Dream are thought to be the product of the mortal minds imagination" + '\n' + "However, a finite mind cannot comprehend the infinite" + '\n'+ "Dreams exsist as a gateway to another world where creatures roam and thrive" + '\n' + "There are no limits but one crucial rule: Balance, for every Nightmare there must be a Dream" + '\n' + "Something is disrupting the balance, Nightmares are plauging the Human kind"+ '\n' + "YOU ARE THE ONLY ONE WHO CAN HELP")
+    
+  
+else:
+    exit()
+
