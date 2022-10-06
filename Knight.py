2780cca85b214e8da0d32fe7ec6618e051f109f1
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-10-06 11:43:21 -0400

    Milstone 3 Finally able to get variables in knight into Game

--- Knight.py
+++ Knight.py
@@ -4,5 +4,6 @@ class KnightAtri:
         Intelligeence = 5
         Experience = 2
         print("Your strength is", strength, "Your Intelligeence is", Intelligeence, "Your Experience is", Experience)
-
+from App import Role
+if Role == 1:
     
