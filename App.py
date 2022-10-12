57fe6255ae40a05b0367713f2d2eebe9b22ba077
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-10-12 00:45:46 -0400

    Final

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import Game
 print("Welcome to Wonderland" + '\n' + "Where Dreams and Nightmares Thrive")
 playGame = input("Dare to enter? (Type yes or no) ")
 if playGame == "yes":
-    print('\n' + "Nightmares and Dream are thought to be the product of the mortal minds imagination" + '\n' + "However, a finite mind cannot comprehend the infinite" + '\n'+ "Dreams exsist as a gateway to another world where creatures roam and thrive" + '\n' + "There are no limits but one crucial rule: Balance, for every Nightmare there must be a Dream" + '\n' + "Something is disrupting the balance, Nightmares are plauging the Human kind"+ '\n' + "YOU ARE THE ONLY ONE WHO CAN HELP")
+    print('\n' + "Nightmares and Dream are thought to be the product of imagination" + '\n' + "in reality however,dreams exsist as a gateway to another world where creatures roam and thrive" + '\n' + "There are no limits but one crucial rule: Balance, for every Nightmare there must be a Dream" + '\n' + "A dragon is disrupting the balance, Nightmares are plauging the Wonderland"+ '\n' + "YOU ARE THE ONLY ONE WHO CAN HELP")
     
     name = input("What is your name (Type in your name) ")           #this code is used to get information from the user
     print('\n',"Hello great hero", name, sep=" ")         #instead of using plus and adding spacing sep helped make it concise
